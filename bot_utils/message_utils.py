2ab589127c90b15820419f7fee3c013678960513
Evgen <zhenyagolenko92@gmail.com>
2025-06-26 12:23:48 +0300

    Реализация обработчика на кнопку подтверждения заказа(h10_confirm_order), функция подсчета количества товаров в корзине(message_utils), функция получения товаров из итоговой корзины пользвателя(utils), зарегистрировал обработчик в диспетчере(main)

--- bot_utils/message_utils.py
+++ bot_utils/message_utils.py
@@ -1,3 +1,6 @@
+from database.utils import db_get_final_cart_items
+
+
 def text_for_caption(name, description, price):
     """Текст для подписи изображения."""
     return (
@@ -5,3 +8,22 @@ def text_for_caption(name, description, price):
         f"Описание: {description}\n"
         f"Стоимость: {price:.2f} BYN"
     )
+
+
+def counting_products_from_cart(chat_id, user_text):
+    """"Функция подсчета количества товаров в корзине"""
+
+    products = db_get_final_cart_items(chat_id)
+
+    if products:
+        text = f'<b>{user_text}</b>\n\n'
+        total_products = total_price = count = 0
+        for name, quantity, price, cart_id in products:
+            count += 1
+            total_products += quantity
+            total_price += price
+            text += f'<b>{count}. {name}</b>\n<b>Количество:</b> {quantity}\n<b>Стоимость:</b> {price}BYN.\n\n'
+        text += (f'<b>Общее количество продуктов:</b> {total_products}\n<b>Общая стоимость :</b> '
+                 f'{total_price}BYN.')
+        context = (count, text, total_price, cart_id)
+        return context
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 from os import getenv
 from dotenv import load_dotenv
+import os
 
 
 load_dotenv()
 TOKEN = getenv('TOKEN')
+
+MANAGER_ID = int(os.getenv('MANAGER_ID', 0))
